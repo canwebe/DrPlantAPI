ee243a6843e602151b77e533a4abaed09a29579b
kaushikd207 <kaushikd207@gmail.com>
2022-07-02 00:07:03 +0530

    added mongodb connection but not working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://kaushikd:database@cluster0.r90ag.mongodb.net/?retryWrites=true&w=majority'
+const url = 'mongodb+srv://disease:database@cluster0.ucprhxb.mongodb.net/?retryWrites=true&w=majority'
 const bodyParser = require("body-parser")
 const app = express()
 
@@ -12,7 +12,7 @@ mongoose.connect(url, {
 }).then(() => {
     console.log('connection successful');
 }).catch((err) => {
-    console.log('no connection')
+    console.log(err)
 })
 const con = mongoose.connection
 
